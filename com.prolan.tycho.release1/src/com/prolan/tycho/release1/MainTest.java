983caea3e047f891156c498cab08423c8f201fa8
Fekete Agnes <Fekete.Agnes@prolan.hu>
2018-08-02 19:38:51 +0200

    agi

--- com.prolan.tycho.release1/src/com/prolan/tycho/release1/MainTest.java
+++ com.prolan.tycho.release1/src/com/prolan/tycho/release1/MainTest.java
@@ -1,11 +1,16 @@
 package com.prolan.tycho.release1;
+import java.util.List;
+import java.util.ArrayList;
+
 import com.prolan.tycho.release2.*;
 
 public class MainTest {
 	
 	public static void main(String[] args) {
+		 List<String> supplierNames1 = new ArrayList<String>();
 		
 		System.out.println(new Test2().writeString());
-	}
+		
 
+}
 }
